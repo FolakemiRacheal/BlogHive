defaf245f86626cf5e5da3c4fca97705c5e19054
unknown <atolagbefolakerach@gmail.com>
2025-08-19 13:01:46 +0100

    almost done

--- blog/models.py
+++ blog/models.py
@@ -11,9 +11,30 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
+    def total_likes(self):
+        return self.likes.count()
+
+
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk':self.pk})
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.post.title}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, related_name="likes", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView,UpdateView, DeleteView
-from .models import Post
+from django.contrib.auth.models import User
+from .models import Post, Comment, Like
 
 # Create your views here.
 
@@ -23,6 +25,20 @@ class PostListView(ListView):
     context_object_name='posts'
     ordering = ['-date_posted']
     paginate_by = 2
+
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name='posts'
+    # ordering = ['-date_posted']
+    paginate_by = 4
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
@@ -56,7 +72,7 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
             return True
         return False
     
-    class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin, DeleteView):
         model = Post
         success_url = '/'
 
@@ -70,3 +86,34 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
 
 def about(request):
     return render(request, 'blog/about.html')
+
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    is_liked = False
+    if request.user.is_authenticated:
+        is_liked = Like.objects.filter(post=post, user=request.user).exists()
+    
+    if request.method == "POST":
+        content = request.POST.get("content")
+        if content:
+            Comment.objects.create(post=post, user=request.user, content=content)
+            return redirect("post_detail", pk=post.pk)
+
+    return render(request, "blog/post_detail.html", {
+        "post": post,
+        "comments": comments,
+        "is_liked": is_liked,
+    })
+
+@login_required
+def toggle_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    like = Like.objects.filter(post=post, user=request.user)
+    if like.exists():
+        like.delete()
+    else:
+        Like.objects.create(post=post, user=request.user)
+    return redirect("post_detail", pk=pk)
